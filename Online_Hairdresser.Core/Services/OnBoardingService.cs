835592bedae1d5884fe4ac0aaf55f178ea1258d8
fatihmandirali <fatih.mandirali@hotmail.com>
2023-05-24 23:26:35 +0300

    onboarding service edited

--- Online_Hairdresser.Core/Services/OnBoardingService.cs
+++ Online_Hairdresser.Core/Services/OnBoardingService.cs
@@ -10,21 +10,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Online_Hairdresser.Models.Models.Response.Theme;
 
 namespace Online_Hairdresser.Core.Services
 {
     public class OnBoardingService : Repository<Onboarding>, IOnBoardingService
     {
         private readonly IMapper _mapper;
-        public OnBoardingService(OnlineHairdresserDbContext context, IMapper mapper) : base(context)
+        private readonly IThemeService _themeService;
+        public OnBoardingService(OnlineHairdresserDbContext context, IMapper mapper, IThemeService themeService) : base(context)
         {
             _mapper = mapper;
+            _themeService = themeService;
         }
 
         public async Task<List<OnboardingResponse>> OnBoardingList()
         {
+            var themeList = await _themeService.FindBy(x => x.IsActive).AsNoTracking().ToListAsync();
             var onBoarding = await FindBy(x => x.IsActive).AsNoTracking().OrderBy(x => x.Id).ToListAsync();
             var mapOnBoarding = _mapper.Map<List<OnboardingResponse>>(onBoarding);
+            foreach (var item in mapOnBoarding)
+            {
+                var theme=themeList.FirstOrDefault(y => y.Gender == item.Gender);
+                item.Theme = _mapper.Map<ThemeResponse>(theme);
+            }
             return mapOnBoarding;
         }
 
--- Online_Hairdresser.Models/Models/Response/Onboarding/OnboardingResponse.cs
+++ Online_Hairdresser.Models/Models/Response/Onboarding/OnboardingResponse.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Online_Hairdresser.Models.Enums;
+using Online_Hairdresser.Models.Models.Response.Theme;
 
 namespace Online_Hairdresser.Models.Models.Response.Onboarding
 {
@@ -12,6 +13,7 @@ namespace Online_Hairdresser.Models.Models.Response.Onboarding
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+        public ThemeResponse Theme { get; set; }
         public GenderEnum Gender { get; set; }
     }
 }
